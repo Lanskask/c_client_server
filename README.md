04281108ee4d586e2c4d5fbfc45b3280ce584d90
Lanskask <grey.wooden.tractor@gmail.com>
2020-05-22 00:19:43 +0300

    First commit. Before changing server implementation

---
+++ README.md
@@ -0,0 +1,6 @@
+# C_Client_Server
+
+
+## TODO
+- one client - read data from file - input_file.txt
+- on server - write message from client into file - message log
---
+++ client.c
@@ -0,0 +1,62 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+
+char *read_file(char file_name[]) {
+    FILE *fp;
+    static char buff[255];
+
+    fp = fopen(file_name, "r");
+    fscanf(fp, "%s", buff);
+    return buff;
+}
+
+void client_server_interaction(int sockfd) {
+    char *buff = read_file("input_file.txt");
+
+    write(sockfd, buff, sizeof(buff));
+    bzero(buff, sizeof(buff));
+    read(sockfd, buff, sizeof(buff));
+    printf("From Server : %s", buff);
+}
+
+int main() {
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and varification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    } else
+        printf("connected to the server..\n");
+
+    // function for chat
+    client_server_interaction(sockfd);
+
+    // close the socket
+    close(sockfd);
+}
